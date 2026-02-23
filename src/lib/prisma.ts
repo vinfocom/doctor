64286b514d1b6f35f4efe579c205f15715cfc045
vinfocom <vinfocom83@gmail.com>
2026-02-23 12:22:04 +0530

    update code

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -15,10 +15,24 @@ function createPrismaClient() {
   return new PrismaClient({ adapter });
 }
 
-const prisma = globalForPrisma.prisma ?? createPrismaClient();
+function getPrismaClient() {
+  if (globalForPrisma.prisma) {
+    return globalForPrisma.prisma;
+  }
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  const client = createPrismaClient();
+  if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = client;
+  }
+
+  return client;
 }
+
+const prisma = new Proxy({} as PrismaClient, {
+  get(_target, prop, receiver) {
+    const client = getPrismaClient();
+    return Reflect.get(client as unknown as object, prop, receiver);
+  },
+});
 
 export default prisma;
